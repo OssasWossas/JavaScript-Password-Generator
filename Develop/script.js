703e133a9699757a7bcd92420665a8c6d6ef5637
OssasWossas <muramba55@hotmail.com>
2021-06-19 18:27:55 +0800

    Added variables and created script to collect information

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var upperL = letters.toLocaleUpperCase();
+var numbers = "0123456789";
+var special_characters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var UC = confirm("Would you like UpperCase Letters?");
+  var LC = confirm("Would you like LowerCase Letters?");
+  var N = confirm("Would you like Numbers?");
+  var special = confirm("Would you like Special Characters?");
 
-  passwordText.value = password;
+  if(!UC && !LC && !N && !special){
+    alert("Need to choose atleast one you bum");
+    console.log(UC + LC + N + special);
+  }
+  else{
+    return;
+  }
+
+  var length = parseInt(prompt("Enter the Length of Character in the password"));
+  if ((length >= 8) && (length <= 128)){
+    console.log("lur a spaz");
+  }
+  else{
+    alert("needs to be between 8 and 128")
+    var length = 0;
+  } 
+  
+  console.log(length);
+
 
 }
 
